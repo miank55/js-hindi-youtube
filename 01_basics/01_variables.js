3ae12555ae6e1145413a2db77f0c5d19bc5809b9
miank55 <mayankkumarsingh.55@gmail.com>
2025-09-07 18:52:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail="mayank@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+
+// accountId=2 // not allowed 
+
+
+accountEmail ="hc@hc.com"
+accountPassword="21212121"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var 
+because of iissue in block scope and functional scope
+*/
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using node js not browsers
+
+console.log(3
+    +
+     3)// code readibility-that should be high
+
+
+console.log("mayank")
+
+let name ="mayank"
+let age = 18
+let isLoggedIn=false
+
+//number=> 2 to power 53
+// bigint
+// string=>""
+//boolean=> true/false
+// null => standalone value
+// undefined => stand alone and undefined 
+// symbol=> unique
+
+
+// object
+ console.log(typeof null);
+ 
+
+    
+
