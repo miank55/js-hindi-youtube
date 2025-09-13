4930e78e722009b4ee2774aeb20892367ea5c145
miank55 <mayankkumarsingh.55@gmail.com>
2025-09-13 09:09:08 +0000

    datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//********************************OPERATIONS *********************************
+
+
+let value =3
+let negValue=-value
+//consloe.log(2+2);
+//consloe.log(2-2);
+//consloe.log(2*2);
+//consloe.log(2**2);
+//consloe.log(2/2);
+//consloe.log(2%2);
+
+
+let str1= "hello"
+let str2="mayank"
+let str3= str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log(3+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let  gameCounter =100
+++gameCounter;
+console.log(gameCounter);
